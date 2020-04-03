6d133784ea57eb87dfba3fcdb0dcff0b67cdb169
kimberly <kimberlyeslami98@gmail.com>
2020-04-03 19:53:37 +0100

    Created AdapterPosts, ModelPost and menu_main

--- app/src/main/java/com/example/alumnguide/AddPostActivity.java
+++ app/src/main/java/com/example/alumnguide/AddPostActivity.java
@@ -18,7 +18,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
--- app/src/main/java/com/example/alumnguide/ForumFragment.java
+++ app/src/main/java/com/example/alumnguide/ForumFragment.java
@@ -3,6 +3,8 @@ package com.example.alumnguide;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -11,16 +13,52 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.alumnguide.adapters.AdapterPosts;
+import com.example.alumnguide.models.ModelPost;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ForumFragment extends Fragment {
+    RecyclerView recyclerView;
+    List<ModelPost> postList;
+    AdapterPosts adapterPosts;
     Button addPost;
+    FirebaseAuth firebaseAuth;
+
+    public ForumFragment() {
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_forum,container,false);
+        //init
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //recycler view and its properties
+        recyclerView = view.findViewById(R.id.postsRecyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager((getActivity()));
+        //show newest post first, for this load from last
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+
+        //set layout to recycler view
+        recyclerView.setLayoutManager(layoutManager);
+
+        //innit post list
+        postList = new ArrayList<>();
+        loadPosts();
 
         addPost = view.findViewById(R.id.addPost_btn);
         addPost.setOnClickListener(new View.OnClickListener() {
@@ -32,4 +70,37 @@ public class ForumFragment extends Fragment {
         });
         return view;
     }
+
+    private void loadPosts() {
+        //path of all posts
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+        //get all data from this ref
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                postList.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+                    postList.add(modelPost);
+
+                    //adapter
+                    adapterPosts = new AdapterPosts(getActivity(),postList);
+                    //set adapter to recycler view
+                    recyclerView.setAdapter(adapterPosts);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            // in case of error
+                Toast.makeText(getActivity(), ""+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void searchPosts(String searchQuery){
+
+    }
+
 }
